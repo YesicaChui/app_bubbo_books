35bdf19ff89d9adb0fcfd70dbd7081c13251abbf
YesicaChui <yesicachuic@gmail.com>
2024-01-10 18:00:13 -0500

    agregando modal para insertar libro

---
+++ src/components/FormularioModal.tsx
@@ -0,0 +1,56 @@
+import { Button, Modal, StyleSheet, Text, TextInput, View } from 'react-native'
+import React from 'react'
+interface FormularioModalProps {
+  isVisible: boolean,
+  setIsVisible:React.Dispatch<boolean>
+}
+const FormularioModal:React.FC<FormularioModalProps> = ({isVisible,setIsVisible}) => {
+  return (
+    <Modal
+    visible={isVisible}
+      transparent={true}
+    >
+      <View style={styles.modalContainer}>
+        <View style={styles.modalContent}>
+          <Text>Agregar Libro</Text>
+          <Text>Titulo</Text>
+          <TextInput placeholder='Ingrese el Titulo' style={styles.input}/>
+          <Text>Autor</Text>
+          <TextInput placeholder='Ingrese el Titulo' style={styles.input}/>
+          <Text>Año</Text>
+          <TextInput placeholder='Ingrese el Titulo' style={styles.input}/>
+          <Button title='Insertar' onPress={()=>setIsVisible(false)}/>
+        </View>
+
+      </View>
+    </Modal>
+  )
+}
+
+export default FormularioModal
+
+const styles = StyleSheet.create({
+  modalContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "rgba(255,255,255,0.8)"
+  },
+  modalContent: {
+    width: "80%",
+    borderRadius: 10,
+    padding: 10,
+    gap: 10,
+    backgroundColor: "#00d26a",
+  },
+  input:{
+    borderWidth: 3,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    width: 150,
+    borderRadius:5,
+    borderColor: "green",
+    fontSize:16,
+    backgroundColor:"orange"
+  }
+})
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,46 +1,38 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Button, Pressable, StyleSheet, Text, View } from 'react-native'
 import ListBooks from '../components/ListBooks'
 import { useEffect, useState } from 'react'
 import { Book } from '../interfaces/book'
+import FormularioModal from '../components/FormularioModal'
 
-const dataBooks:Book[] =[
-  {
-    "id": "44otUZjGZPgk1HoePv0Z",
-    "year": "2015",
-    "author": "Jorge Espinoza",
-    "title": "HTML"
-  },
-  {
-    "id": "98iO3tMfWUeiOMWHiILp",
-    "author": "Alex Marin",
-    "year": "2020",
-    "title": "Flowise IA"
-  },
-  {
-    "id": "qfjW9GYA8pQ427sDU6jo",
-    "year": "2023",
-    "author": "Karen",
-    "title": "CSS"
-  }
-]
 const HomeScreen = () => {
   const [books, setBooks] = useState<Book[]>([])
-  useEffect(()=>{
+  const [isVisible, setIsVisible]=useState(false)
+
+  useEffect(() => {
     //lectura de libros de api
     fetch("http://192.168.0.14:3002/books")
-    .then(res=>res.json())
-    .then(data=>  setBooks(data))
-    .catch(error=>console.log("mierror",error))
+      .then(res => res.json())
+      .then(data => setBooks(data))
+      .catch(error => console.log("mierror", error))
 
-  },[])
+  }, [])
   return (
     <View>
-    <Text>App Books Bubbo</Text>
-    <ListBooks books={books}/>
-  </View>
+      <Text>App Books Bubbo</Text>
+      <Pressable style={styles.boton_agregar} onPress={() => setIsVisible(true)}>
+        <Text>Agregar</Text>
+      </Pressable>
+      <FormularioModal isVisible={isVisible} setIsVisible={setIsVisible}/>
+      <ListBooks books={books} />
+    </View>
   )
 }
 
 export default HomeScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  boton_agregar: {
+    backgroundColor: "green"
+
+  }
+})
